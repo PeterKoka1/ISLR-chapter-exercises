165294e996b3aff8359adc3aa892b9a4c82da404
Peter Kokalov <petervkokalov@gmail.com>
2017-11-09 00:57:13 -0500

    Update Chapter7.R

--- Chapter7.R
+++ Chapter7.R
@@ -59,3 +59,74 @@ fit <- glm(wage ~ cut(age, 8), data = Wage)
 preds <- predict(fit, newdata = list(age = age.grid))
 lines(age.grid, preds, col = "red", lwd = 2)
 title("Step Function with Cutpoints = C = 8")
+
+attach(Auto)
+pairs(Auto)
+# wage looks like it can be modelled by displacement, horsepower and weight
+
+# Polynomial Regresion
+cv.errors <- rep(NA, 15)
+for (i in 1:length(cv.errors)) {
+  fit <- glm(mpg ~ poly(horsepower, i), data = Auto)
+  cv.errors[i] <- cv.glm(Auto, fit, K = 10)$delta[1]
+}
+xrange <- seq(from = 1, to = length(cv.errors))
+cv.errors
+plot(xrange, cv.errors, type = "l")
+which.min(cv.errors)
+points(6, cv.errors[6], col = "red", pch = 20, cex = 2)
+
+poly.fit <- glm(mpg ~ poly(horsepower, 6), data = Auto)
+horselims <- range(horsepower)
+horse.grid <- seq(from = horselims[1], to = horselims[2])
+poly.pred <- predict(poly.fit, newdata = list(horsepower = horse.grid))
+
+# Step Functions
+cv.errors <- rep(NA, 20)
+for (i in 2:length(cv.errors)) {
+  Auto$horsepower.cut <- cut(Auto$horsepower, i)
+  fit <- glm(mpg ~ horsepower.cut, data = Auto)
+  cv.errors[i] <- cv.glm(Auto, fit, K = 10)$delta[1]
+}
+xrange <- seq(from = 2, to = length(cv.errors))
+plot(xrange, cv.errors[!(is.na(cv.errors))], 
+     xlab = "Number of Cuts", ylab = "Test MSE", type = "l")
+which.min(cv.errors)
+points(15, cv.errors[15], col = "red", pch = 20, cex = 2)
+
+step.fit <- glm(mpg ~ cut(horsepower, 15), data = Auto)
+step.pred <- predict(step.fit, newdata = list(horsepower = horse.grid))
+
+# Spline Functions
+library(splines)
+cv.errors <- rep(NA, 15)
+for (i in 1:length(cv.errors)) {
+  spline.fit <- glm(mpg ~ ns(horsepower, df = i), data = Auto)
+  cv.errors[i] <- cv.glm(Auto, spline.fit, K = 10)$delta[1]
+}
+xrange <- seq(from = 1, to = 15)
+plot(xrange, cv.errors, xlab = "Degrees of Freedom", ylab = "Test MSE", type = "l")
+which.min(cv.errors)
+points(10, cv.errors[10], col = "red", pch = 20, cex = 2)
+
+ns.spline.fit <- lm(mpg ~ ns(horsepower, df = 10), data = Auto)
+spline.pred <- predict(ns.spline.fit, newdata = list(horsepower = horse.grid))
+
+par(mfrow=c(1,3))
+plot(mpg ~ horsepower, data = Auto, col = "darkgrey", main = "Polynomial Regression")
+lines(horse.grid, poly.pred, col = "red", lwd = 2)
+plot(mpg ~ horsepower, data = Auto, col = "darkgrey", main = "Step Function")
+lines(horse.grid, step.pred, col = "blue", lwd = 2)
+plot(mpg ~ horsepower, data = Auto, col = "darkgrey", main = "Natural Spline w/ 10 Degrees of Freedom")
+lines(horse.grid, spline.pred, col = "red", lwd = 2)
+
+attach(Boston)
+set.seed(123)
+fit <- glm(nox ~ poly(dis, 3), data = Boston)
+dislim <- range(dis)
+dis.grid <- seq(from = dislim[1], to = dislim[2], by = 0.01)
+preds <- predict(fit, newdata = list(dis = dis.grid))
+par(mfrow=c(1,1))
+plot(nox ~ dis, data = Boston, col = "darkgray", 
+     main = "Distance vs. Nitrogen Oxides Concentration")
+lines(dis.grid, preds, col = "red", lwd = 3)
